9285c3699a1875f4aaf08b23217f48d9cccae859
Madhurya <madhuryan@boston-technology.com>
2020-08-03 19:30:21 +0530

    no message

--- participant-manager-module/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/config/AppPropertyConfig.java
+++ participant-manager-module/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/config/AppPropertyConfig.java
@@ -9,21 +9,17 @@
 package com.google.cloud.healthcare.fdamystudies.config;
 
 import java.io.Serializable;
-import lombok.Getter;
-import lombok.Setter;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Configuration
 @Setter
 @Getter
 public class AppPropertyConfig implements Serializable {
-
-  @Value("${clientId}")
-  private String clientId;
-
-  @Value("${secretKey}")
-  private String secretKey;
 
   @Value("${auth.server.url}")
   private String authServerUrl;

d4dc64d8f99987355cda0ded1cc64a50520cd4bd
aswinijena100 <ashwinikumarj@boston-technology.com>
2020-09-18 12:15:53 +0530

    Added Initial template for Integration Test cases
    Added Initial template for Integration Test cases

---
+++ WCP/fdahpStudyDesigner/src/test/java/com/fdahpstudydesigner/controller/test/UsersControllerTest.java
@@ -0,0 +1,33 @@
+package com.fdahpstudydesigner.controller.test;
+
+import javax.servlet.ServletContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:TestApplicationContext.xml"})
+@WebAppConfiguration
+public class UsersControllerTest {
+
+  @Autowired private WebApplicationContext webApplicationContext;
+
+  protected MockMvc mockMvc;
+
+  @Before
+  public void setUp() throws Exception {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+  }
+
+  @Test
+  public void viewUserDetailsSuccess() throws Exception {
+    ServletContext servletContxt = webApplicationContext.getServletContext();
+  }
+}

fd0a013a39236b204486cdba65482ab3b5d7c9bc
prakashm181 <prakashm@boston-technology.com>
2020-09-25 09:49:22 +0530

    Merge branch 'develop' into participant-manager-webapp-import-email-list

--- UR-web-app/src/app/site-coordinator/sites/shared/site-details.service.ts
+++ UR-web-app/src/app/site-coordinator/sites/shared/site-details.service.ts
@@ -4,13 +4,14 @@ import {EntityService} from '../../../service/entity.service';
 import {Observable} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '@environment';
+import {AddEmail} from './add-email';
 import {
   InviteSend,
   StatusUpdate,
   UpdateInviteResponse,
 } from '../../participant-details/participant-details';
 import {ApiResponse} from 'src/app/entity/api.response.model';
-
+import {OnboardingStatus} from 'src/app/shared/enums';
 @Injectable({
   providedIn: 'root',
 })
@@ -20,19 +21,28 @@ export class SiteDetailsService {
     private readonly entityService: EntityService<SiteParticipants>,
     private readonly http: HttpClient,
   ) {}
-  get(siteId: string, fetchingOption: string): Observable<SiteParticipants> {
-    const fetchingOptions =
-      fetchingOption === 'all'
-        ? ''
-        : fetchingOption === 'new'
-        ? '?onboardingStatus=N'
-        : fetchingOption === 'invited'
-        ? '?onboardingStatus=I'
-        : '?onboardingStatus=D';
-    return this.entityService.get(
-      `sites/${siteId}/participants` + fetchingOptions,
-    );
+
+  get(
+    siteId: string,
+    fetchingOption: OnboardingStatus,
+  ): Observable<SiteParticipants> {
+    const fetchingOptions = this.getInvitationType(fetchingOption);
+    if (fetchingOption !== OnboardingStatus.All) {
+      return this.http.get<SiteParticipants>(
+        `${environment.baseUrl}/sites/${encodeURIComponent(
+          siteId,
+        )}/participants`,
+        {
+          params: {onboardingStatus: fetchingOptions},
+        },
+      );
+    } else {
+      return this.entityService.get(
+        `sites/${encodeURIComponent(siteId)}/participants`,
+      );
+    }
   }
+
   siteDecommission(siteId: string): Observable<ApiResponse> {
     return this.http.put<ApiResponse>(
       `${environment.baseUrl}/sites/${encodeURIComponent(siteId)}/decommission`,
@@ -76,5 +86,27 @@ export class SiteDetailsService {
       formData,
       httpOptionsForUpload,
     );
+  }
+  addParticipants(
+    siteId: string,
+    modelEmail: AddEmail,
+  ): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(
+      `${environment.baseUrl}/sites/${encodeURIComponent(siteId)}/participants`,
+      modelEmail,
+    );
+  }
+
+  private getInvitationType(fetchingOption: OnboardingStatus): string {
+    switch (fetchingOption) {
+      case OnboardingStatus.New:
+        return 'N';
+      case OnboardingStatus.Invited:
+        return 'I';
+      case OnboardingStatus.Disabled:
+        return 'D';
+      default:
+        return '';
+    }
   }
 }

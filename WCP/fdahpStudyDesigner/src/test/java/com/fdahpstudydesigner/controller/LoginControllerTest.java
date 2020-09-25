6163dc8f2b631c7e12ec78e3361be532a0618951
harisboston <69199888+harisboston@users.noreply.github.com>
2020-09-25 18:43:53 +0530

    Added Copyright
    Added Copyright

--- WCP/fdahpStudyDesigner/src/test/java/com/fdahpstudydesigner/controller/LoginControllerTest.java
+++ WCP/fdahpStudyDesigner/src/test/java/com/fdahpstudydesigner/controller/LoginControllerTest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 package com.fdahpstudydesigner.controller;
 
 import static com.fdahpstudydesigner.common.StudyBuilderAuditEvent.USER_SIGNOUT_SUCCEEDED;
--- WCP/fdahpStudyDesigner/src/test/java/com/fdahpstudydesigner/controller/StudyControllerTest.java
+++ WCP/fdahpStudyDesigner/src/test/java/com/fdahpstudydesigner/controller/StudyControllerTest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 package com.fdahpstudydesigner.controller;
 
 import static com.fdahpstudydesigner.common.StudyBuilderAuditEvent.LAST_PUBLISHED_VERSION_OF_STUDY_VIEWED;

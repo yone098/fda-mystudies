df97f58981d785aee006a65d2edacdd240e4f4ba
prakashm181 <prakashm@boston-technology.com>
2020-09-30 12:41:22 +0530

    reverse proxy fix

--- UR-web-app/src/environments/environment.prod.ts
+++ UR-web-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseUrl: '/api/participant-scim-service',
-  loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
+  loginUrl: 'https://35.222.67.4/oauth2/auth',
   authServerUrl: '/api/oauth-scim-service',
   redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/callback',
   clientId: 'MYSTUDIES_OAUTH_CLIENT',

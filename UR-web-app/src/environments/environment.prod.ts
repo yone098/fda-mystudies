d037fd32300cac62bcb2826c4e182dc2d3638949
prakashm181 <prakashm@boston-technology.com>
2020-10-03 14:08:51 +0530

    enviornment changes

--- UR-web-app/src/environments/environment.prod.ts
+++ UR-web-app/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  baseUrl: '/api/participant-manager-service',
+  baseUrl: 'https://35.193.185.224:8096/api/participant-manager-service',
   loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
-  authServerUrl: '/api/oauth-scim-service',
+  authServerUrl: 'https://35.193.185.224:8096/api/oauth-scim-service',
   redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/callback',
   clientId: 'MYSTUDIES_OAUTH_CLIENT',
   appVersion: 'v0.1',

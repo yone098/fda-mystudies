ab1634260b3cee77810f185c5c5d2e2e6d62c8c4
Madhurya <madhuryan@boston-technology.com>
2020-10-12 12:44:06 +0530

    /push notification changes

--- user-registration-server-ws/user-mgmt-module/user-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/dao/CommonDaoImpl.java
+++ user-registration-server-ws/user-mgmt-module/user-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/dao/CommonDaoImpl.java
@@ -209,11 +209,11 @@ public class CommonDaoImpl implements CommonDao {
   }
 
   @Override
-  public Map<Integer, Map<String, JSONArray>> getStudyLevelDeviceToken(
+  public Map<String, Map<String, JSONArray>> getStudyLevelDeviceToken(
       List<StudyEntity> studyInfos) {
     logger.info("CommonDaoImpl.getStudyLevelDeviceToken() - starts");
 
-    Map<Integer, Map<String, JSONArray>> studyDeviceTokenMap = new HashMap<>();
+    Map<String, Map<String, JSONArray>> studyDeviceTokenMap = new HashMap<>();
     Session session = this.sessionFactory.getCurrentSession();
 
     if (studyInfos != null && !studyInfos.isEmpty()) {
@@ -233,7 +233,7 @@ public class CommonDaoImpl implements CommonDao {
       if (rs != null) {
         for (Object[] objects : rs) {
 
-          Integer studyId = (Integer) objects[0];
+          String studyId = (String) objects[0];
           String deviceToken = (String) objects[1];
           String deviceType = (String) objects[2];
           if (deviceToken != null) {

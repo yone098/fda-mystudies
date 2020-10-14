c8a63b61a9ac23c79b88782368f82813999ff1d4
Madhurya <madhuryan@boston-technology.com>
2020-10-14 16:09:29 +0530

    logo image

--- participant-manager-module/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/service/SiteServiceImpl.java
+++ participant-manager-module/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/service/SiteServiceImpl.java
@@ -1109,6 +1109,7 @@ public class SiteServiceImpl implements SiteService {
       }
 
       studyDetail.setSitesCount((long) studyDetail.getSites().size());
+      studyDetail.setLogoImageUrl(study.getLogoImageUrl());
       studies.add(studyDetail);
     }
 

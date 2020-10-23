2b23ddf2d54a573b9e71e4ab64b0d6fac1d4e1ba
harisboston <69199888+harisboston@users.noreply.github.com>
2020-10-23 11:46:42 +0530

    fixed review comment for 1377
    fixed review comment for 1377

--- oauth-scim-module/oauth-scim-service/src/test/java/com/google/cloud/healthcare/fdamystudies/oauthscim/common/PasswordGeneratorTest.java
+++ oauth-scim-module/oauth-scim-service/src/test/java/com/google/cloud/healthcare/fdamystudies/oauthscim/common/PasswordGeneratorTest.java
@@ -11,14 +11,24 @@ package com.google.cloud.healthcare.fdamystudies.oauthscim.common;
 import static org.junit.Assert.assertTrue;
 
 import com.google.cloud.healthcare.fdamystudies.common.PasswordGenerator;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class PasswordGeneratorTest {
 
   @ParameterizedTest
-  @ValueSource(ints = {0, 4, 8, 10, 12})
-  public void isValidPassword(int passwordLength) {
+  @ValueSource(ints = {8, 10, 12})
+  public void genaratePassword(int passwordLength) {
+    Set<String> passwords = new HashSet<>();
+    for (int i = 0; i < 100; i++) {
+      String password = genarateUniqueValidPassword(passwordLength);
+      assertTrue(passwords.add(password));
+    }
+  }
+
+  public String genarateUniqueValidPassword(int passwordLength) {
     String password = PasswordGenerator.generate(passwordLength);
 
     boolean lowerCase = false;
@@ -38,14 +48,13 @@ public class PasswordGeneratorTest {
       }
     }
 
-    if (passwordLength == 0) {
-      assertTrue("should return empty value", passwordLength == 0);
-    } else {
-      assertTrue("Password should contain alteast one digit", numeric);
-      assertTrue("Password should contain alteast one lowercase letter", lowerCase);
-      assertTrue("Password should contain alteast one uppercase letter", upperCase);
-      assertTrue("Password should contain alteast one special character", specialChar);
-      assertTrue("Password length mismatched", password.length() == passwordLength);
-    }
+    assertTrue("Password length should not be zero", password.length() != 0);
+    assertTrue("Password should contain alteast one digit", numeric);
+    assertTrue("Password should contain alteast one lowercase letter", lowerCase);
+    assertTrue("Password should contain alteast one uppercase letter", upperCase);
+    assertTrue("Password should contain alteast one special character", specialChar);
+    assertTrue("Password length mismatched", password.length() == passwordLength);
+
+    return password;
   }
 }

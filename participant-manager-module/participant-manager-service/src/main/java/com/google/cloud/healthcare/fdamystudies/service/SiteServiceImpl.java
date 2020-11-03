98d4d1d7c57d4c22beed0cd7a2374072e4faffbe
Chiranjibi009 <chiranjibid@boston-technology.com>
2020-11-03 20:03:26 +0530

    issue 1711 changes revoked

--- participant-manager-module/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/service/SiteServiceImpl.java
+++ participant-manager-module/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/service/SiteServiceImpl.java
@@ -1204,6 +1204,7 @@ public class SiteServiceImpl implements SiteService {
                 ? Permission.NO_PERMISSION.value()
                 : permission);
       }
+
       List<SitePermissionEntity> sitePermissionList =
           sitePermissionRepository.findByUserIdAndStudyId(userId, study.getId());
 
@@ -1313,48 +1314,51 @@ public class SiteServiceImpl implements SiteService {
       StudyEntity study,
       StudyDetails studyDetail,
       List<SitePermissionEntity> sitePermissions) {
+
     Map<String, Long> enrolledInvitedCountForOpenStudyBySiteId =
         getEnrolledCountForOpenStudyGroupBySiteId(study);
 
     for (SitePermissionEntity sitePermissionEntity : sitePermissions) {
-      EnrolledInvitedCount enrolledInvitedCount =
-          enrolledInvitedCountMap.get(sitePermissionEntity.getSite().getId());
+      if (sitePermissionEntity.getSite().getStatus().equals(SiteStatus.ACTIVE.value())) {
+        EnrolledInvitedCount enrolledInvitedCount =
+            enrolledInvitedCountMap.get(sitePermissionEntity.getSite().getId());
 
-      Long invitedCount = 0L;
-      Long enrolledCount = 0L;
-      if (enrolledInvitedCount != null) {
-        invitedCount = enrolledInvitedCount.getInvitedCount();
-        enrolledCount = enrolledInvitedCount.getEnrolledCount();
+        Long invitedCount = 0L;
+        Long enrolledCount = 0L;
+        if (enrolledInvitedCount != null) {
+          invitedCount = enrolledInvitedCount.getInvitedCount();
+          enrolledCount = enrolledInvitedCount.getEnrolledCount();
+        }
+
+        SiteDetails site = new SiteDetails();
+        site.setId(sitePermissionEntity.getSite().getId());
+        site.setName(sitePermissionEntity.getSite().getLocation().getName());
+
+        String studyType = study.getType();
+        if (studyType.equals(OPEN_STUDY)
+            && sitePermissionEntity.getSite().getTargetEnrollment() != null) {
+          site.setEnrolled(
+              MapUtils.isNotEmpty(enrolledInvitedCountForOpenStudyBySiteId)
+                  ? enrolledInvitedCountForOpenStudyBySiteId.get(
+                      sitePermissionEntity.getSite().getId())
+                  : 0L);
+          site.setInvited(Long.valueOf(sitePermissionEntity.getSite().getTargetEnrollment()));
+        } else if (studyType.equals(CLOSE_STUDY)) {
+          site.setInvited(invitedCount);
+          site.setEnrolled(enrolledCount);
+        }
+
+        if (site.getInvited() != 0 && site.getInvited() >= site.getEnrolled()) {
+          Double percentage =
+              (Double.valueOf(site.getEnrolled()) * 100) / Double.valueOf(site.getInvited());
+          site.setEnrollmentPercentage(percentage);
+        } else if (site.getInvited() != 0
+            && site.getEnrolled() >= site.getInvited()
+            && studyType.equals(OPEN_STUDY)) {
+          site.setEnrollmentPercentage(DEFAULT_PERCENTAGE);
+        }
+        studyDetail.getSites().add(site);
       }
-
-      SiteDetails site = new SiteDetails();
-      site.setId(sitePermissionEntity.getSite().getId());
-      site.setName(sitePermissionEntity.getSite().getLocation().getName());
-
-      String studyType = study.getType();
-      if (studyType.equals(OPEN_STUDY)
-          && sitePermissionEntity.getSite().getTargetEnrollment() != null) {
-        site.setEnrolled(
-            MapUtils.isNotEmpty(enrolledInvitedCountForOpenStudyBySiteId)
-                ? enrolledInvitedCountForOpenStudyBySiteId.get(
-                    sitePermissionEntity.getSite().getId())
-                : 0L);
-        site.setInvited(Long.valueOf(sitePermissionEntity.getSite().getTargetEnrollment()));
-      } else if (studyType.equals(CLOSE_STUDY)) {
-        site.setInvited(invitedCount);
-        site.setEnrolled(enrolledCount);
-      }
-
-      if (site.getInvited() != 0 && site.getInvited() >= site.getEnrolled()) {
-        Double percentage =
-            (Double.valueOf(site.getEnrolled()) * 100) / Double.valueOf(site.getInvited());
-        site.setEnrollmentPercentage(percentage);
-      } else if (site.getInvited() != 0
-          && site.getEnrolled() >= site.getInvited()
-          && studyType.equals(OPEN_STUDY)) {
-        site.setEnrollmentPercentage(DEFAULT_PERCENTAGE);
-      }
-      studyDetail.getSites().add(site);
     }
   }
 

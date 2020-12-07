b436b2b4d4393d67d6fe90b465b874efec3792c9
navya-BTC <navyat@boston-technology.com>
2020-12-07 12:34:26 +0530

    get Studies pagination

--- participant-manager-module/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/StudyController.java
+++ participant-manager-module/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/StudyController.java
@@ -63,9 +63,10 @@ public class StudyController {
       @RequestHeader(name = USER_ID_HEADER) String userId,
       @RequestParam(defaultValue = "999999999") Integer limit,
       @RequestParam(defaultValue = "0") Integer offset,
+      @RequestParam(required = false) String searchTerm,
       HttpServletRequest request) {
     logger.entry(String.format(BEGIN_REQUEST_LOG, request.getRequestURI()));
-    StudyResponse studyResponse = studyService.getStudies(userId, limit, offset);
+    StudyResponse studyResponse = studyService.getStudies(userId, limit, offset, searchTerm);
     logger.exit(String.format(STATUS_LOG, studyResponse.getHttpStatusCode()));
     return ResponseEntity.status(studyResponse.getHttpStatusCode()).body(studyResponse);
   }

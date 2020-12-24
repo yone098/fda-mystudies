c3db8af45136ed9d6141e84eae601885cf6f1d41
Chiranjibi009 <chiranjibid@boston-technology.com>
2020-12-24 12:14:55 +0530

    issue 2439 fixes

--- user-registration-server-ws/user-mgmt-module/user-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/service/UserRegistrationServiceImpl.java
+++ user-registration-server-ws/user-mgmt-module/user-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/service/UserRegistrationServiceImpl.java
@@ -196,7 +196,7 @@ public class UserRegistrationServiceImpl implements UserRegistrationService {
     EmailResponse emailResponse = sendConfirmationEmail(userDetails, verificationCode);
     if (MessageCode.EMAIL_ACCEPTED_BY_MAIL_SERVER.getMessage().equals(emailResponse.getMessage())) {
       userDetails.setEmailCode(verificationCode);
-      userDetails.setCodeExpireDate(Timestamp.valueOf(LocalDateTime.now().plusMinutes(expireTime)));
+      userDetails.setCodeExpireDate(Timestamp.valueOf(LocalDateTime.now().plusHours(expireTime)));
       userDetails = userDetailsRepository.saveAndFlush(userDetails);
     }
     return emailResponse;

727a042c95a0e70ab8e8f2c372372be1a4796380
harisboston <harish@boston-technology.com>
2021-01-04 17:37:01 +0530

    Fixed [mobile] Account lock is not working as expected and blocking other functionalities #1477
    Fixed [mobile] Account lock is not working as expected and blocking other functionalities #1477

--- oauth-scim-module/oauth-scim-service/src/main/java/com/google/cloud/healthcare/fdamystudies/oauthscim/service/UserServiceImpl.java
+++ oauth-scim-module/oauth-scim-service/src/main/java/com/google/cloud/healthcare/fdamystudies/oauthscim/service/UserServiceImpl.java
@@ -384,7 +384,7 @@ public class UserServiceImpl implements UserService {
     }
 
     UserEntity userEntity = optUserEntity.get();
-    JsonNode userInfo = userEntity.getUserInfo();
+    ObjectNode userInfo = (ObjectNode) userEntity.getUserInfo();
 
     JsonNode passwordNode = userInfo.get(PASSWORD);
     if (userEntity.getStatus() == UserAccountStatus.ACCOUNT_LOCKED.getStatus()) {
@@ -395,6 +395,7 @@ public class UserServiceImpl implements UserService {
       } else {
         // unlock the user account
         userEntity.setStatus(UserAccountStatus.ACTIVE.getStatus());
+        userInfo.put(LOGIN_ATTEMPTS, 0);
       }
     }
     String hash = getTextValue(passwordNode, HASH);
@@ -461,12 +462,11 @@ public class UserServiceImpl implements UserService {
   }
 
   private AuthenticationResponse updateInvalidLoginAttempts(
-      UserEntity userEntity, JsonNode userInfoJsonNode, AuditLogEventRequest auditRequest) {
+      UserEntity userEntity, ObjectNode userInfo, AuditLogEventRequest auditRequest) {
     if (userEntity.getStatus() == UserAccountStatus.ACCOUNT_LOCKED.getStatus()) {
       throw new ErrorCodeException(ErrorCode.ACCOUNT_LOCKED);
     }
 
-    ObjectNode userInfo = (ObjectNode) userInfoJsonNode;
     int loginAttempts =
         userInfo.hasNonNull(LOGIN_ATTEMPTS) ? userInfo.get(LOGIN_ATTEMPTS).intValue() : 0;
     loginAttempts += 1;
@@ -505,8 +505,7 @@ public class UserServiceImpl implements UserService {
   }
 
   private AuthenticationResponse updateLoginAttemptsAndAuthenticationTime(
-      UserEntity userEntity, JsonNode userInfoJsonNode, AuditLogEventRequest auditRequest) {
-    ObjectNode userInfo = (ObjectNode) userInfoJsonNode;
+      UserEntity userEntity, ObjectNode userInfo, AuditLogEventRequest auditRequest) {
     userInfo.put(LOGIN_TIMESTAMP, Instant.now().toEpochMilli());
 
     UserAccountStatus status = UserAccountStatus.valueOf(userEntity.getStatus());
@@ -539,7 +538,7 @@ public class UserServiceImpl implements UserService {
   }
 
   private void validatePasswordExpiryAndAccountStatus(
-      UserEntity userEntity, JsonNode userInfo, AuditLogEventRequest auditRequest) {
+      UserEntity userEntity, ObjectNode userInfo, AuditLogEventRequest auditRequest) {
     JsonNode passwordNode =
         userEntity.getStatus() == UserAccountStatus.ACCOUNT_LOCKED.getStatus()
             ? userInfo.get(ACCOUNT_LOCKED_PASSWORD)

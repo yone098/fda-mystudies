a1b359ba13b7b798e0181793bf0323d2bc2a78cc
prakashm181 <prakashm@boston-technology.com>
2021-01-05 19:17:29 +0530

    error message update
    error message update

--- participant-manager/src/app/shared/generic.error.codes.enum.ts
+++ participant-manager/src/app/shared/generic.error.codes.enum.ts
@@ -3,7 +3,7 @@ const genericErrorMessages = {
   EC_0034:
     'This link is no longer valid to be used. Please contact the system admin for assistance with your account or sign in if already registered.',
   EC_0080:
-    'Your session has been terminated as a security measure, either due to inactivity or an account update. Please sign in again to continue.',
+    'Sorry, an error has occurred and your request could not be processed. Please try again later.',
 
   /* eslint-enable @typescript-eslint/naming-convention */
 };
